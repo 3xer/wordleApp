a22ebc05835cf0e7dd9a0e6936cc2bbe69b60b6e
3xer <pontuslundholm1@gmail.com>
2023-04-19 15:21:27 +0200

    algorithm

---
+++ algorithm.js
@@ -0,0 +1,45 @@
+export default function wordle(guess, rightAnswer) {
+  const input = guess.toUpperCase().split("");
+  let answer = rightAnswer.toUpperCase().split("");
+  let outArray = guess.toUpperCase().split("");
+  let correctLetters = [];
+  for (let i = 0; i < input.length; i++) {
+    const inCorrect = {
+      "letter": input[i],
+      "result": "incorrect",
+    };
+    const correct = {
+      "letter": input[i],
+      "result": "correct",
+    };
+    if (answer.includes(input[i])) {
+      if (outArray[i] == answer[i]) {
+        outArray.splice(i, 1, correct);
+        correctLetters.push(input[i]);
+      }
+    } else {
+      outArray.splice(i, 1, inCorrect);
+    }
+  }
+  for (let i = 0; i < outArray.length; i++) {
+    const inCorrect = {
+      "letter": input[i],
+      "result": "incorrect",
+    };
+    if (correctLetters.includes(outArray[i])) {
+      outArray.splice(i, 1, inCorrect);
+    }
+  }
+  for (let i = 0; i < outArray.length; i++) {
+    const missplaced = {
+      "letter": input[i],
+      "result": "missplaced",
+    };
+    if (answer.includes(outArray[i])) {
+      outArray.splice(i, 1, missplaced);
+    }
+  }
+  console.log(out)
+  return outArray;
+}
+//wordle('guess', 'guess')
